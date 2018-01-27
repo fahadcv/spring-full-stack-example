085df10bee76988712790100833c3b0b861841e2
fahadcv <fahadcv@gmail.com>
2018-01-27 20:23:18 +0530

    Added password encryption to Spring Security

--- src/main/java/com/fhd/devopsbuddy/backend/service/UserService.java
+++ src/main/java/com/fhd/devopsbuddy/backend/service/UserService.java
@@ -8,6 +8,7 @@ import com.fhd.devopsbuddy.backend.persistence.repositories.RoleRepository;
 import com.fhd.devopsbuddy.backend.persistence.repositories.UserRepository;
 import com.fhd.devopsbuddy.enums.PlansEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,8 +26,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
     @Transactional
     public User createUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles) {
+        String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
+
         Plan plan = new Plan(plansEnum);
         // It makes sure the plans exist in the database
         if (!planRepository.exists(plansEnum.getId())) {
